c3221d02358405b0459fe52e0bc1805b13c71337
Denis Sel <sfaiross@yandex.ru>
2019-11-10 10:26:38 +0200

    Calculete logic is added

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,43 @@ import Buttons from './components/Buttons';
 import './App.css';
 
 function App() {
-  const [result, setResult] = useState(0);
+  const [result, setResult] = useState('');
+
+  function reset(){
+    setResult('');
+  }
+
+  function backspace(){
+    setResult(result.slice(0, -1));
+  }
+
+  function calculate() {
+    // eslint-disable-next-line
+    setResult(eval(result).toFixed(2));
+  }
+
+  function calculateResult(val) {
+    if(val === "C") {
+      reset()
+    } else if(result.length > 15) {
+      setResult("Error: many symbols");
+    } else if(val === "=") {
+      calculate();
+    } else if(val === "CE"){
+      backspace()
+    } else {
+      setResult(result + val);
+    }
+  }
+
+
 
   return (
       <div>
         <div className="calculator">
           <h1>My Calculator</h1>
           <Result result={result}/>
-          <Buttons onClick={(val) => setResult(val)}/>
+          <Buttons onClick={calculateResult}/>
         </div>
       </div>
     );

2670f0d6538dd8993182efb8f4c56f9bca1dd9c7
Denis Sel <sfaiross@yandex.ru>
2019-11-10 11:21:15 +0200

    Updates

---
+++ precache-manifest.e582c387f78a7c758bbd2cd1a3df84fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4cecdfb82dade2ce4c466a1e6ae3e22",
+    "url": "/ReactCalculator/index.html"
+  },
+  {
+    "revision": "aae32239d791cbc05e2c",
+    "url": "/ReactCalculator/static/css/main.60f61988.chunk.css"
+  },
+  {
+    "revision": "de87f4df0e507be357e8",
+    "url": "/ReactCalculator/static/js/2.89d157dc.chunk.js"
+  },
+  {
+    "revision": "aae32239d791cbc05e2c",
+    "url": "/ReactCalculator/static/js/main.99aab0ba.chunk.js"
+  },
+  {
+    "revision": "e430cabdabc8d5fa692e",
+    "url": "/ReactCalculator/static/js/runtime-main.373b7318.js"
+  }
+]);
